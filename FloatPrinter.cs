4e7854879479dd220cb5e65534fa3567823d9776
anitaorji <74414041+anitaorji@users.noreply.github.com>
2025-04-11 12:04:36 -0700

    Complete

---
+++ FloatPrinter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsExample
+{
+    class FloatPrinter
+    {
+        private float _data;
+        public FloatPrinter(float data)
+        {
+            _data = data;
+        }
+        public float PrintData()
+        {
+            return _data;
+        }
+    }
+}
